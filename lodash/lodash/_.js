91c8c8e2e38f4d307981aeaf79e6ab0102365e6b
B-T-D <bdye71@gmail.com>
2021-03-12 21:21:04 -0500

    Stopping lodash clone project for now, seems egregiously large and unchallenging

--- lodash/lodash/_.js
+++ lodash/lodash/_.js
@@ -5,8 +5,38 @@ const _ = {
         number = Math.max(lower, number);
         number = Math.min(upper, number);
         return number;
+    },
+    inRange(number, start=0, end) {
+        if (!end) {
+            end = start;
+            start = 0;
+        }
+        if (start > end) {
+            var temp = start;
+            start = end;
+            end = temp;
+        }
+        return (number >= start && number < end);
+
+
+    },
+    words(string) {
+        arr = [];
+        var s = [];
+        for (let i = 0; i < string.length; i++) {
+            var char = string[i];
+            if (char === ' ') {
+                arr.push(s.join(''));
+                s = [];
+            } else {
+                s.push(char);
+            }
+
+        }
+        arr.push(s.join(''));
+        return arr;
     }
-    
+
 };
 
 
