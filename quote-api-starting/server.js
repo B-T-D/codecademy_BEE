de008b5f4b4c5dedcae6aa13d8855d0dda6a5481
B-T-D <bdye71@gmail.com>
2021-03-24 19:49:26 -0400

    Completed quote API project

--- quote-api-starting/server.js
+++ quote-api-starting/server.js
@@ -8,3 +8,49 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes', (req, res, next) => {
+    let responseBody = {
+        quotes: []
+    }
+    if (!req.query.person) {
+        quotes.forEach(quote => {
+            responseBody.quotes.push(quote);
+        })
+    } else {
+        quotes.forEach(quote => {
+            if (quote.person === req.query.person) {
+                responseBody.quotes.push(quote);
+            }
+        })
+    }
+    
+    res.send(responseBody);
+})
+
+app.get('/api/quotes/random', (req, res, next) => {
+    let responseBody = {
+        quote: {}
+    }
+    const selectedQuote = getRandomElement(quotes);
+    responseBody.quote = selectedQuote;
+    res.send(responseBody);
+})
+
+app.post('/api/quotes', (req, res, next) => {
+    const quote = req.query.quote;
+    const person = req.query.person;
+    if ((!quote) || (!person)) {
+        res.status(400).send()
+    } else {
+        quotes.push({
+            'quote': quote,
+            'person': person
+        })
+        res.status(201).send();
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
+
